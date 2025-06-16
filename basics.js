911178c51756469e8540b0ae96f2e015b91c3ff8
Deep Sorathiya <deep.professional803@gmail.com>
2025-06-16 22:32:34 +0530

    Add files via upload

---
+++ basics.js
@@ -0,0 +1,21 @@
+let a = 5;
+console.log(a);
+
+a = 9;
+console.log(a);
+console.log(typeof a);
+
+let b = 10;
+console.log("a = "+ a);
+console.log("b = "+ b);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+console.log(typeof "deep");
+console.log(typeof 'deep');
+console.log(typeof '123');
