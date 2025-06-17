8f00e77d14c15bc296514e639a2b06623a8042af
Deep Sorathiya <deep.professional803@gmail.com>
2025-06-17 23:06:33 +0530

    Add files via upload

---
+++ Basic.ts
@@ -0,0 +1,17 @@
+// let a = "deep:;
+a = 5;
+console.log(a);
+console.log(typeof a);
+
+//a = true; // error occures so that in TypeScript we are not able to changed our variable type
+
+
+let name = "Ashadeep";
+
+console.log(`your name is ${name}`);
+
+console.log(typeof name);
+
+name = "deep"; // no errors
+
+//name = true; // error
