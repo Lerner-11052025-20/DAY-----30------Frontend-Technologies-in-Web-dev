bdedd1e76fd61282541290bb81370c06304c2f4d
Deep Sorathiya <deep.professional803@gmail.com>
2025-06-18 23:01:48 +0530

    Add files via upload
    coding....

---
+++ app4.js
@@ -0,0 +1,16 @@
+// let number = prompt("Enter a number :- ");
+// number = parseInt(number);
+// console.log(number);
+
+// number = parseInt(prompt("Enter a number :- "));
+// console.log(number);
+// if (number == NaN)
+//     console.error("please enter a number");
+// else 
+//     console.warn(`your enter number is ${number} `);
+
+
+console.log(`welcome ${5+ " " +5}`);
+console.log(`welcome ${5+5}${' '}deep`);
+console.log("welcome", 5," ",5, 5+5);
+console.log("welcome", 5, " " ,5, 5+5);
