25f5e5acb820108fc21ea261e758df02aa47f6b5
Deep Sorathiya <deep.professional803@gmail.com>
2025-06-18 23:01:26 +0530

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,14 @@
+let a = 5;
+console.log(a);
+let flag = true;
+let b = a ** 8;
+console.log("deep", flag, a, b);
+
+console.log(a+b);
+console.log("your answer is this => " +(a+b));
+console.log("your answer is this => ", a+b);
+console.log(`your answer is this => ${a+b}`);
+console.log(`your answer is this =>${a+b}`);
+console.log(`your answer is this =>${' '}${a+b}$`);
+console.log(`your answer is this =>${a}$${' '}${b}$`);
+
