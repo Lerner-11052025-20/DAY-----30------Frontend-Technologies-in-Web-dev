14662208067994678f61487dfbfbaf9f7ba2b3f9
Deep Sorathiya <deep.professional803@gmail.com>
2025-06-20 23:29:51 +0530

    Add files via upload

---
+++ slice.js
@@ -0,0 +1,17 @@
+let colors = ["red", "green", "blue", "yellow"];
+console.log(colors);
+
+console.log(colors.slice(0));
+console.log(colors.slice(1,4));
+console.log(colors.slice(1,3));
+console.log(colors.slice(1));
+console.log(colors.slice(1, colors.length));
+console.log(colors.slice(1, colors.length-1));
+console.log(colors.slice(1, colors.length-2));
+console.log(colors.slice(0, 0));
+console.log(colors.slice(0, 1));
+console.log(colors.slice(-1));
+console.log(colors);
+console.log(colors.slice(-2));
+console.log(colors.slice(0-colors.length));
+console.log(colors.slice(7));
