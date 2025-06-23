f1d6f50e4e4182f2d8834e9200448c57b6b3a98b
Deep Sorathiya <deep.professional803@gmail.com>
2025-06-23 22:58:59 +0530

    Add files via upload

---
+++ basicFuncation.js
@@ -0,0 +1,14 @@
+function printHello()
+{
+    console.log("Hello World!!!");
+}
+
+for(let i=1 ; i<=10 ; i++)  printHello();
+
+function dice()
+{
+    console.log(Math.floor(Math.random() * 6) + 1);
+}
+
+for(let i=1 ; i<=10 ; i++)  dice();
+dice();
